1ed81004cee768e5750d8b07197ce597d9a64f53
jronnomo <ggronnii@gmail.com>
2023-08-19 06:27:18 -0400

    commit add new todos feature to web app

--- web.py
+++ web.py
@@ -1,21 +1,23 @@
 import streamlit as st
 import functions
 
-st.title("Todo App")
-st.subheader("This is my todo app")
-st.write("Some random")
-
 todos = functions.get_todos()
 
-new_todo = st.text_input(label="Todo Input Label", placeholder="Add a todo here", label_visibility='hidden')
+
+def add_todo():
+    todo = st.session_state["new_todo"]
+    todos.append(todo + '\n')
+    functions.write_todos(todos)
 
 
-while True:
-    if new_todo != "":
-        try:
-            todos.append(new_todo)
-            functions.write_todos(todos)
-            st.checkbox(new_todo)
-        except:
-            pass
+st.title("Todo App")
+st.subheader("List of todos:")
 
+for todo in todos:
+    st.checkbox(todo)
+
+new_todo = st.text_input(label="Todo Input Label",
+                         placeholder="Add a todo here",
+                         label_visibility='hidden',
+                         on_change=add_todo,
+                         key="new_todo")
